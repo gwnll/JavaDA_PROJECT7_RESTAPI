fadf314987c75031f9bed577d3bd46e53084c196
gwenn <gwenaelle.montfort@live.fr>
2022-01-12 22:59:12 +0100

    Add IT Test

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/BidListIT.java
@@ -0,0 +1,41 @@
+package com.nnk.springboot.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith({SpringExtension.class})
+public class BidListIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void testGetFormBid() throws Exception {
+        mockMvc.perform(get("/bidList/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("bidList/add"));
+    }
+
+    @Test
+    @WithMockUser
+    public void validateBidSuccess() throws Exception {
+        mockMvc.perform(post("/bidList/validate"))
+                .andExpect(status().isOk());
+    }
+
+
+}
